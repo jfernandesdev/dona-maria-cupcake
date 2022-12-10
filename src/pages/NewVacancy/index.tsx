ed3800a3ab766ba7a8b86e1771b6933182400411
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-10 16:40:35 -0300

    STYLE: 'new vacancy' form style with yup validation

--- src/pages/NewVacancy/index.tsx
+++ src/pages/NewVacancy/index.tsx
@@ -1,7 +1,162 @@
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import uuid from 'react-uuid'
+
+import Swal from 'sweetalert2'
+
+import styles from './styles.module.scss'
+import { NavLink } from 'react-router-dom'
+
+const newVacancyFormSchema = yup.object({
+  jobTitle: yup.string().required(),
+  wage: yup.number().required(),
+  jobActivity: yup.string().required(),
+  benefits: yup.string().required(),
+  processSteps: yup.string().required(),
+  skills: yup.string().required(),
+  experienceRequired: yup.string().required(),
+})
+
+type NewVacancyFormInputs = yup.InferType<typeof newVacancyFormSchema>
+
 export function NewVacancy () {
+  const { 
+    register,  
+    handleSubmit, 
+    reset, 
+    formState: { errors, isSubmitting }
+  } = useForm<NewVacancyFormInputs>({
+    resolver: yupResolver(newVacancyFormSchema),
+    // defaultValues: {
+    //   jobTitle: 'Title Job',
+    //   wage: 2000,
+    //   jobActivity: 'Atividade 1; Atividade 2',
+    //   benefits: 'Benefício 1; Benefício 2',
+    //   processSteps: 'Etapa 1, Etapa 2',
+    //   skills: 'Skill 1, Skill 2',
+    //   experienceRequired: 'Experience 1, Experience 2'
+    // }
+  })
+
+  async function handleNewVacancy(data: NewVacancyFormInputs) {
+    const { jobTitle, wage, jobActivity, benefits, processSteps, skills, experienceRequired } = data
+
+    const newVacancy = {
+      id: uuid(),
+      jobTitle,
+      wage,
+      jobActivity,
+      benefits,
+      processSteps,
+      skills,
+      experienceRequired,
+      creationDate: new Date(), 
+    }
+
+    console.log(newVacancy)
+
+    reset()
+
+    Swal.fire({
+      title: 'Sucesso!',
+      text: 'Vaga cadastrada com sucesso!',
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 2500,
+      timerProgressBar: true,
+    })
+  }
+
   return (
-    <>
-      <h1>New vacancy</h1>
-    </>
+    <div>
+      <h1>Cadastro de nova vaga</h1>
+
+      <form onSubmit={handleSubmit(handleNewVacancy)} className={styles.form}>
+        <div className={styles.inputsInline}>
+          <label htmlFor="jobTitle">
+            <span>Título do cargo</span>
+            <input 
+              id="jobTitle" 
+              placeholder="Informe o título do cargo"
+              {...register('jobTitle')}
+              className={errors.jobTitle ? styles.inputErrorAlert : ''}
+            />
+          </label>
+
+          <label htmlFor="wage">
+            <span>Salário</span>
+            <div className={`${styles.inputWithPrefix} ${errors.wage ? styles.inputErrorAlert : ''}`}>
+              <span>R$</span>
+              <input 
+                type="number"
+                id="wage"
+                placeholder="0.00" 
+                {...register('wage', { valueAsNumber: true})} 
+              />
+            </div>
+          </label>
+        </div>
+
+        <label htmlFor="jobActivity">
+          <span>Atividades que o cargo exercerá</span>
+          <textarea 
+            id="jobActivity" 
+            {...register('jobActivity')}
+            className={errors.jobActivity ? styles.inputErrorAlert : ''}
+          /> 
+        </label>
+
+        <label htmlFor="benefits">
+          <span>Benefícios do cargo</span>
+          <textarea
+            id="benefits" 
+            {...register('benefits')}
+            className={errors.benefits ? styles.inputErrorAlert : ''}
+          />
+        </label>
+
+        <label htmlFor="processSteps">
+          <span>Etapas do processo</span>
+          <textarea 
+            id="processSteps" 
+            {...register('processSteps')}
+            className={errors.processSteps ? styles.inputErrorAlert : ''}
+          />
+        </label>
+
+        <label htmlFor="skills">
+          <span>Habilidades necessárias</span>
+          <textarea 
+            id="skills" 
+            {...register('skills')}
+            className={errors.skills ? styles.inputErrorAlert : ''}
+          />
+        </label>
+
+        <label htmlFor="experienceRequired">
+          <span>Experiência necessária</span>
+          <textarea 
+            id="experienceRequired"
+            {...register('experienceRequired')} 
+            className={errors.experienceRequired ? styles.inputErrorAlert : ''}
+          />
+        </label>
+
+        <footer>
+
+          <NavLink to="/">
+            <button type="reset">
+              Cancelar
+            </button>
+          </NavLink>
+
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Cadastrando...' : 'Cadastrar nova vaga'}
+          </button>
+        </footer>
+      </form>
+    </div>
   )
 }
