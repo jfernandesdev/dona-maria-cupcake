85fc28f36a441181e84778e3c82048741bd61c57
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-10 07:49:15 -0300

    STYLE: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { EnvelopeSimple } from 'phosphor-react' 
+
+import AvatarDonaMaria from '../../assets/avatar-dona-maria.png'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.container}>
+
+      <button className={styles.notificationButton} aria-label="notification">
+        <EnvelopeSimple size={32} weight="light"/>
+      </button>
+
+      <div className={styles.profileInformation}>
+        <img 
+          src={AvatarDonaMaria} 
+          className={styles.avatar}
+          alt="Dona Maria"
+        />
+        <div>
+          <span className={styles.name}>
+            Dona Maria
+          </span>
+          <span className={styles.email}>
+            donamaria@email.com
+          </span>
+        </div>
+      </div>
+    </header>
+  )
+}
