6e13bd175487dc73ab89bc00df16554f9b999918
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-11 14:10:07 -0300

    FEAT: benefits multiple selection field

--- src/pages/NewVacancy/index.tsx
+++ src/pages/NewVacancy/index.tsx
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
-import uuid from 'react-uuid'
-import Swal from 'sweetalert2'
-
+import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
+import uuid from 'react-uuid'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+import Swal from 'sweetalert2'
+
 import { processStepsOptions } from '../../utils/processStepsOptions'
 import { experienceRequiredOptions } from '../../utils/experienceRequiredOptions'
+import { benefitsOptions } from '../../utils/benefitsOptions'
 
 import styles from './styles.module.scss'
 
@@ -15,7 +18,7 @@ const newVacancyFormSchema = yup.object({
   jobTitle: yup.string().required(),
   wage: yup.number().required(),
   jobActivity: yup.string().required(),
-  benefits: yup.string().required(),
+  benefits: yup.array().of(yup.string()).required(),
   processSteps: yup.array().of(yup.string()),
   skills: yup.string().required(),
   experienceRequired: yup.string().required(),
@@ -24,24 +27,29 @@ const newVacancyFormSchema = yup.object({
 type NewVacancyFormInputs = yup.InferType<typeof newVacancyFormSchema>
 
 export function NewVacancy () {
-  const { 
-    register,  
-    handleSubmit,
-    reset, 
-    formState: { errors, isSubmitting }
-  } = useForm<NewVacancyFormInputs>({
+  const methods = useForm<NewVacancyFormInputs>({
     resolver: yupResolver(newVacancyFormSchema),
     defaultValues: {
       jobTitle: 'Title Job',
       wage: 2000,
       jobActivity: 'Atividade 1; Atividade 2',
-      benefits: 'Benefício 1; Benefício 2',
       processSteps: ["Inscrição", "Resultado Final"],
+      benefits: [''],
       skills: 'Skill 1, Skill 2',
       experienceRequired: 'Sem necessidade'
     }
   })
 
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isSubmitting }
+  } = methods;
+
+  const animatedComponents = makeAnimated();
+  
   async function handleNewVacancy(data: NewVacancyFormInputs) {
     const { jobTitle, wage, jobActivity, benefits, processSteps, skills, experienceRequired } = data
 
@@ -76,6 +84,25 @@ export function NewVacancy () {
       <h1>Cadastro de nova vaga</h1>
 
       <form onSubmit={handleSubmit(handleNewVacancy)} className={styles.form}>
+        <label htmlFor="processSteps">
+          <span>Selecione as etapas do processo</span>
+          <div className={styles.processStepsContainer}>
+            {processStepsOptions.map((option) => (
+              <div key={option.key} className={styles.inputCheckbox}>
+                <input
+                  type="checkbox"
+                  id={option.key}
+                  value={option.value}
+                  {...register('processSteps')}
+                />
+                <label htmlFor={option.key}>
+                  {option.value}
+                </label>
+              </div>
+            ))}
+          </div>
+        </label>
+        
         <div className={styles.inputsInline}>
           <label htmlFor="jobTitle">
             <span>Título do cargo</span>
@@ -101,55 +128,53 @@ export function NewVacancy () {
           </label>
         </div>
 
-        <label htmlFor="jobActivity">
-          <span>Atividades que o cargo exercerá</span>
-          <textarea 
-            id="jobActivity" 
-            {...register('jobActivity')}
-            className={errors.jobActivity ? styles.inputErrorAlert : ''}
-          /> 
-        </label>
-
         <label htmlFor="benefits">
           <span>Benefícios do cargo</span>
-          <textarea
-            id="benefits" 
-            {...register('benefits')}
-            className={errors.benefits ? styles.inputErrorAlert : ''}
-          />
-        </label>
+          <Controller
+            control={control}
+            name='benefits'
+            render={({ field: { onChange, value, ref } }) => (
+              <>
+                {console.log(value)}
 
-        <label htmlFor="processSteps">
-          <span>Selecione as etapas do processo</span>  
-          <div className={styles.processStepsContainer}>
-            {processStepsOptions.map((option) => (
-              <div key={option.key} className={styles.inputCheckbox}>
-                <input
-                  type="checkbox"
-                  id={option.key}
-                  value={option.value}
-                  {...register('processSteps')}
+                <Select
+                  ref={ref}
+                  value={benefitsOptions.filter(c => value.includes(c.value))}
+                  onChange={val => onChange(val.map(c => c.value))}
+                  options={benefitsOptions}
+                  isMulti
+                  closeMenuOnSelect={false}
+                  components={animatedComponents}
+                  placeholder="Digite algo e aperte enter (ou selecione)..."
+                  classNamePrefix="select"
                 />
-                <label htmlFor={option.key}>
-                  {option.value}
-                </label>
-              </div>
-            ))}
-          </div>
-        </label>
-
-        <label htmlFor="skills">
-          <span>Habilidades necessárias</span>
-          <textarea 
-            id="skills" 
-            {...register('skills')}
-            className={errors.skills ? styles.inputErrorAlert : ''}
+              </>
+            )}
           />
         </label>
+
+        <div className={styles.flex}>
+          <label htmlFor="skills">
+            <span>Habilidades necessárias</span>
+            <textarea
+              id="skills"
+              {...register('skills')}
+              className={errors.skills ? styles.inputErrorAlert : ''}
+            />
+          </label>
+
+          <label htmlFor="jobActivity">
+            <span>Atividades que o cargo exercerá</span>
+            <textarea
+              id="jobActivity"
+              {...register('jobActivity')}
+              className={errors.jobActivity ? styles.inputErrorAlert : ''}
+            />
+          </label> 
+        </div>
 
         <label htmlFor="experienceRequired">
           <span>Experiência necessária</span>
-
           <div className={styles.customCheckbox}>
             {experienceRequiredOptions.map((option) => (
               <div key={option.key}>
---
+++ src/utils/benefitsOptions.ts
@@ -0,0 +1,14 @@
+export const benefitsOptions = [
+  { value: 'Vale-alimentação', label: 'Vale-alimentação' },
+  { value: 'Vale-refeição', label: 'Vale-refeição' },
+  { value: 'Vale-transporte', label: 'Vale-transporte' },
+  { value: 'Gympass', label: 'Gympass' },
+  { value: 'Auxílio-Creche', label: 'Auxílio-Creche' },
+  { value: 'Assistência Médica', label: 'Assistência médica' },
+  { value: 'Assistência Odontológica', label: 'Assistência odontológica' },
+  { value: 'Seguro de Vida', label: 'Seguro de vida' },
+  { value: 'Participação nos Lucros', label: 'Participação nos lucros' },
+  { value: 'Cursos/Treinamento', label: 'Cursos/Treinamento' },
+  { value: 'Jornada Flexível', label: 'Jornada Flexível' },
+  { value: 'Day-off', label: 'Day-off' },
+]
