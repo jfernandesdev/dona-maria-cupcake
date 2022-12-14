1587c56b4323ccaec8b96ffaa0db82162e1748d0
jfernandesdev <jfernandes.dev@gmail.com>
2022-12-14 11:37:42 -0300

    FIX: container spacing on mobile devices

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { House, Briefcase, SignOut } from 'phosphor-react'
+import { NavLink as NavLinkReact } from 'react-router-dom'
 import { NavLink } from './NavLink'
 
 import IconCupcake from '../../assets/icon-cupcake.svg'
@@ -8,8 +9,10 @@ import styles from './styles.module.scss'
 export function Sidebar() {
   return (
     <nav className={styles.container}>
-      <img src={IconCupcake} className={styles.brandLogo} alt="Cupcake Dona Maria" />
-
+      <NavLinkReact to='/'>
+        <img src={IconCupcake} className={styles.brandLogo} alt="Cupcake Dona Maria" />
+      </NavLinkReact>
+      
       <ul>
         <NavLink href='/' icon={<House/>}  tooltip="Home"/>
         <NavLink href='/nova-vaga' icon={<Briefcase />} tooltip="Cadastrar nova vaga" />
